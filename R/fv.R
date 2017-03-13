75b11901f93a1694478ea3bac7f9fc7f1625eac2
Emil Ahlmann Østergaard <eao@schantz.com>
2017-03-13 15:34:33 +0100

    added description

--- R/fv.R
+++ R/fv.R
@@ -1,8 +1,22 @@
-fv <- function(r,n,pv=0,pmt=0,pmtPrimo=TRUE){
-  if(typeof(pmtPrimo)!= "logical"){
-    print("pmtPrimo must be boolian" )
+#' Returns the future value of an investment (fv)
+#'
+#' @param rate The interest rate per period. Default is zero. Must be entered as decimal
+#' @param nper The total number of payment periods. Default is one period
+#' @param pv The present value of future payments. Default is assumed to be zero. Must be entered as a negative number
+#' @param pmt The payment made each period (annuity). Must be entered as a negative number.
+#' @param pmtUltimo When payments are due. TRUE = end of period, FALSE = beginning of period. Default is TRUE.
+#' @seealso \code{\link{fv.simple}}
+#' @seealso \code{\link{fv.annuity}}
+#' @export
+#' @examples
+#' fv(rate=0.01,nper=10,pv==1000,pmt=10,pmtUltimo=TRUE)
+
+
+fv <- function(rate=0,nper=1,pv=0,pmt=0,pmtUltimo=TRUE){
+  if(typeof(pmtUltimo)!= "logical"){
+    print("pmtUltimo must be boolian" )
   }else{
-    if(isTRUE(pmtPrimo)){adjustment=0 }else{adjustment=1}
-    return(fv.single(r,n,pv) + fv.annuity(r,n,pmt,pmtPrimo))
+    if(isTRUE(pmtUltimo)){adjustment=0 }else{adjustment=1}
+    return(fv.single(rate,nper,pv) + fv.annuity(rate,nper,pmt,pmtUltimo))
   }
 }
--- R/fv.Single.R
+++ R/fv.Single.R
@@ -1,3 +1,15 @@
-fv.single <- function(r,n,pv){
-  return((pv * (1 + r)^n)*(-1))
+#' Estimate future value (fv) of a single sum
+#'
+#' @param r discount rate, or the interest rate at which the amount will be compounded each period
+#' @param n number of periods
+#' @param pv present value
+#' @seealso \code{\link{fv}}
+#' @export
+#' @examples
+#' fv.single(0.08,10,-300)
+#'
+#' fv.single(r=0.04,n=20,pv=-50000)
+
+fv.single <- function(rate,nper,pv){
+  return((pv * (1 + rate)^nper)*(-1))
 }
