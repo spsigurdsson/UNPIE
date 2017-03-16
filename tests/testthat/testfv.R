147aac14a04a0ccbc99fd4da12a34bbc8b51999d
Snorri Sigurdsson <sps@schantz.com>
2017-03-16 17:22:31 +0000

    added another test fixed danish letters in DESCRIPTION

--- tests/testthat/testfv.R
+++ tests/testthat/testfv.R
@@ -1,5 +1,5 @@
 library(unpie)
-
+# devtools:test()
 
 context("fv: Future value")
 
@@ -8,5 +8,15 @@ test_that("01 fv test: init test", {
   r = fv(pv=1000,nper=35,rate=0.04)
   expect_equal(r[35],-3946.089)
   expect_equal(length(r),35)
+})
+
+test_that("02 fv test: init test", {
+  v = 1000
+  r = 0.06
+  res = fv(pv=v,nper=35,rate=r)
+  expect_equal(res[1],-v*(1+r))
+  expect_equal(res[2],-v*(1+r)^2)
+  expect_equal(res[35],-v*(1+r)^35)
+
 })
 
