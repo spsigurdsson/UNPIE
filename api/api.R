b17d23f40b368ec576453593a28f3bf5e50246a8
William Gan <wdg@schantz.com>
2017-06-07 16:19:37 +0200

    added fv.single endpoint

--- api/api.R
+++ api/api.R
@@ -15,3 +15,14 @@ function(rate=0,inflation=0, nper=1,pv=0,pmt=0,pmtinfladj=FALSE, pmtUltimo=TRUE)
   )
 
 }
+
+#* @get /fv.single
+function(rate = 0, inflation = 0, nper = 1, pv = 0){
+  unpie::fv.single(
+    rate = as.numeric(rate),
+    inflation = as.numeric(inflation),
+    nper = as.numeric(nper),
+    pv = as.numeric(pv)
+  )
+  
+}
